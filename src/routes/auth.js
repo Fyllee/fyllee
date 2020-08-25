386f3915f8d851542e1d158e3526530d0547abb6
Mathis Le Bonniec <mathislbonniec@gmail.com>
2020-08-25 22:30:16 +0200

    ✨User registration

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -12,36 +12,36 @@ router.post('/login', async (req, res, next) => {
       return next(err);
 
     if (!user)
-      return res.status(400).json({
-        message: 'Something is not right',
-        user,
-      });
+      return res.status(400).json({ message: 'Something is not right' });
 
     req.login(user, { session: false }, (err2) => {
       if (err2)
         res.send(err2);
 
       const token = jwt.sign(user.toJSON(), process.env.JWT_SECRET);
-      return res.json({ user, token });
+      return res.json({ user: user.toUserData(), token });
     });
   })(req, res, next);
 });
 
-/* eslint-disable no-unused-vars */
-router.post('/register', async (req, res, next) => {
-  // Vérifier qu'il n'existe pas
-  // const user = await User.find({ email: req.body.email });
-  // Vérifier que tous les champs sont ok
-  // L'ajouter à la bdd
+router.post('/register', async (req, res, _next) => {
+  const bodyContainsAllRequired = req.requiredParameters(User);
 
-  // Temporary test
-  // new User({
-  //   name: 'Emmanuel Macron',
-  //   email: 'abc@defg.com',
-  //   password: 'test',
-  // }).save();
+  if (!bodyContainsAllRequired)
+    return res.status(400).json({ message: 'Missing body parameters' });
 
-  res.json({ message: '/auth/register coming soon...' });
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (user)
+      return res.status(409).json({ message: 'User already exists' });
+
+    const newUser = await User.create(req.body);
+
+    return res.json({ user: newUser.getUserData() });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ message: 'Oops... Something went wrong.' });
+  }
 });
 
 export default router;
