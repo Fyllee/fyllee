f2d4bab7af31076a1852a4a291e5c5b308e4b513
Mathis Le Bonniec <mathislbonniec@gmail.com>
2020-08-26 20:13:00 +0200

    🎨 Add excludes fields

--- src/middlewares/requiredParameters.js
+++ src/middlewares/requiredParameters.js
@@ -1,10 +1,18 @@
 export default (req, res, next) => {
   /**
    * @param {Model} model The model to compare the body against
+   * @param {Array | String} exclude Required fields to exclude
    */
-  req.requiredParameters = (model) => {
+  req.requiredParameters = (model, exclude) => {
     const required = model.schema.requiredPaths();
     const body = Object.keys(req.body);
+
+    if (exclude) {
+      if (!Array.isArray(exclude))
+        exclude = [exclude];
+
+      exclude.forEach(e => required.splice(required.indexOf(e), 1));
+    }
 
     return required.every(e => body.includes(e));
   };
