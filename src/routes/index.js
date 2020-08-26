4b40537150348ec080f9d3f8947073576c0e05a2
Mathis Le Bonniec <mathislbonniec@gmail.com>
2020-08-26 15:36:59 +0200

    🚚 Rename api v1 route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import passport from 'passport';
-import apiv1 from './apiv1';
+import v1 from './api/v1';
 import applications from './applications';
 import auth from './auth';
 import user from './user';
@@ -15,6 +15,6 @@ router.get('/', (req, res) => {
 router.use('/user', passport.authenticate('jwt', { session: false }), user);
 router.use('/auth', auth);
 router.use('/applications', passport.authenticate('jwt', { session: false }), applications);
-router.use('/api/v1/', passport.authenticate('jwt', { session: false }), apiv1);
+router.use('/api/v1', passport.authenticate('jwt', { session: false }), v1);
 
 export default router;
