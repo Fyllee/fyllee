2b7875a2ea21f964f602e399811e64056d951d4d
Mathis Le Bonniec <mathislbonniec@gmail.com>
2020-08-27 21:53:25 +0200

    ✨ Add application deletion

--- src/routes/api/v1/applications.js
+++ src/routes/api/v1/applications.js
@@ -2,10 +2,11 @@ import { promises as fs } from 'fs';
 import { join } from 'path';
 import { Router } from 'express';
 import jwt from 'jsonwebtoken';
-import Application from '../models/application';
-import User from '../models/user';
+import Application from '../../../models/application';
+import User from '../../../models/user';
 
 const router = Router();
+const uploadPath = join(process.cwd(), 'public', 'uploads');
 
 router.post('/', async (req, res, _next) => {
   const bodyContainsAllRequired = req.requiredParameters(Application, 'owner');
@@ -23,13 +24,41 @@ router.post('/', async (req, res, _next) => {
       ...req.body,
     });
 
-    await fs.mkdir(join(process.cwd(), 'public', 'uploads', newApp.id));
+    await fs.mkdir(join(uploadPath, newApp.id));
 
     const token = jwt.sign({ id: newApp.id }, process.env.JWT_SECRET);
     return res.json({ message: 'Application created.', application: newApp.toData(), token });
   } catch (err) {
     console.error(err);
     return res.error('Oops... Something went wrong.', 500);
+  }
+});
+
+router.delete('/', async (req, res, _next) => {
+  const { id } = req.body;
+
+  if (!id)
+    return res.error('No id was provided', 400);
+
+  try {
+    const application = await Application.findOne({ id });
+    if (!application)
+      return res.error('Application not found', 404);
+
+    const folderPath = join(uploadPath, application.id);
+    const folder = await fs.stat(folderPath);
+
+    // Delete from storage and database
+    if (folder)
+      await fs.rmdir(folderPath);
+    await Application.deleteOne({ id });
+
+    // TODO: also delete all images inside this app, from database and disk
+
+    res.success('Success!', 200);
+  } catch (err) {
+    console.error(err);
+    return res.error('Something went wrong...', 500);
   }
 });
 
