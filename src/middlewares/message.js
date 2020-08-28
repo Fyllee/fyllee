3a4eb70744b5ed1782072c59d921dfe38109a5c4
Elliot Maisl <elliot.maisl@gmail.com>
2020-08-28 17:47:45 +0200

    🎨 Improve messages middleware and their documentation

--- src/middlewares/message.js
+++ src/middlewares/message.js
@@ -2,15 +2,21 @@ export default (req, res, next) => {
   /**
    * @type {RequestHandler}
    * @param {String} message Message to display to usert
-   * @param {Object} objects Optional objects to display to the user
    * @param {Number | 200} statusCode Status code to return
+   * @param {Object} objects Optional objects to display to the user
    */
   res.success = (message, statusCode = 200, objects = {}) => {
     res.status(statusCode).json({ message, ...objects });
   };
 
+  /**
+   * @type {RequestHandler}
+   * @param {String} message Message to display to usert
+   * @param {Number} statusCode Status code to return
+   * @param {Error} error The error to log in the console
+   */
   res.error = (message, statusCode, error = {}) => {
-    if (Object.keys(error).length !== 0)
+    if (error instanceof Error)
       console.error(error); // Must be replaced by a logger
 
     res.status(statusCode).json({ message, ...error });
