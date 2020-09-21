bb370b62219650556e146cf6bcf16db86c30123c
Mathis Le Bonniec <mathislbonniec@gmail.com>
2020-09-21 19:49:32 +0200

    🐛 Fix boolean issue

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,11 +1,12 @@
+import dotenv from 'dotenv';
 import { Router } from 'express';
 import { login, register } from '../controllers/auth';
 
+dotenv.config();
 const router = Router();
 
 router.post('/login', login);
-
-if (process.env.AUTH_REGISTRATION !== false)
+if (JSON.parse(process.env.AUTH_REGISTRATION) !== false)
   router.post('/register', register);
 
 export default router;
