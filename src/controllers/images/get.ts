8b08d69d5b2e80b5e794996039b7af09b150ffda
Mathis Le Bonniec <mathislbonniec@gmail.com>
2021-02-14 10:39:28 +0100

    🚧 First implementation of filters

--- src/controllers/images/get.ts
+++ src/controllers/images/get.ts
@@ -2,8 +2,9 @@ import { join } from 'path';
 import type { NextFunction, Request, Response } from 'express';
 import constants from '@/app/config/constants';
 import messages from '@/app/config/messages';
-import validateNumber from '@/app/helpers/validate-number';
-import validateRange from '@/app/helpers/validate-range';
+// import validateNumber from '@/app/helpers/validate-number';
+// import validateRange from '@/app/helpers/validate-range';
+import applyFilters from '@/app/helpers/apply-filters';
 import Image from '@/app/models/image';
 
 /**
@@ -25,10 +26,15 @@ export async function getImage(req: Request, res: Response, _next: NextFunction)
       .filter((filtersOptions): filtersOptions is [filter: string, option: string] => typeof filtersOptions[0] === 'string' && typeof filtersOptions[1] === 'string'),
   );
 
-  if (validateNumber(filters?.greyscale) && validateRange(filters.greyscale, 0, 100))
-    console.log('greyscale', filters.greyscale, typeof filters?.greyscale);
+  const path = join(constants.uploadPath, image.application.applicationId, image.savedName);
+  const img = await applyFilters(path, filters); // Get the image btw
 
-  res.sendFile(join(constants.uploadPath, image.application.applicationId, image.savedName));
+  // if (validateNumber(filters?.greyscale) && validateRange(filters.greyscale, 0, 100))
+  //   console.log('greyscale', filters.greyscale, typeof filters?.greyscale);
+
+  // res.sendFile(join(constants.uploadPath, image.application.applicationId, image.savedName));
+  res.set('Content-Type', 'image/png');
+  res.send(img);
 }
 
 /**
