708f69a6ae2482a355dc3b328f802154bf331626
Elliot Maisl <elliot.maisl@gmail.com>
2021-07-02 19:51:04 +0200

    fix(auth-login.dto): remove validation

--- src/auth/dto/auth-login.dto.ts
+++ src/auth/dto/auth-login.dto.ts
@@ -1,14 +1,9 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsAlphanumeric, Length, NotContains } from 'class-validator';
 
 export class AuthLoginDto {
   @ApiProperty({ required: true, example: 'Demo User' })
-  @IsAlphanumeric()
-  @Length(3, 50)
   username: string;
 
   @ApiProperty({ required: true, example: 'demo123' })
-  @NotContains(' ', { message: 'password should not contain a space' })
-  @Length(6, 100)
   password: string;
 }
